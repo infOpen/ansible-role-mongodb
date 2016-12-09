74324e57685447b80d883574ae844a3a0314fa88
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-12-09 06:38:34 +0100

    Update docker testing

--- tests/testinfra/test_installation.py
+++ tests/testinfra/test_installation.py
@@ -1,6 +1,7 @@
 """
 Role tests
 """
+import os
 import pytest
 
 # To run all the tests on given docker images:
@@ -9,34 +10,32 @@ pytestmark = pytest.mark.docker_images(
 )
 
 
-def test_community_repository_file(SystemInfo, File):
+def test_repository_file(SystemInfo, File):
     """
-    Test community repository file permissions
+    Test repository file permissions and content on Ubuntu distributions
     """
 
-    os_distribution = SystemInfo.distribution
+    if SystemInfo.distribution != 'ubuntu':
+        pytest.skip('Not apply to %s' % SystemInfo.distribution)
 
-    if os_distribution == 'ubuntu':
+    if 'org' in os.getenv('MONGODB_EDITION'):
         repo_file_name = '/etc/apt/sources.list.d/mongodb-org.list'
-
-    repo_file = File(repo_file_name)
-
-    assert repo_file.exists
-    assert repo_file.is_file
-    assert repo_file.user == 'root'
-    assert repo_file.group == 'root'
-    assert repo_file.mode == 0o644
-
-
-def test_enterprise_repository_file(SystemInfo, File):
-    """
-    Test enterprise repository file permissions
-    """
-
-    os_distribution = SystemInfo.distribution
-
-    if os_distribution == 'ubuntu':
+        expected_content = (
+            'deb http://repo.mongodb.org/apt/{distribution} '
+            '{release}/mongodb-org/3.2 multiverse'
+        ).format(
+            distribution=SystemInfo.distribution,
+            release=SystemInfo.codename
+        )
+    else:
         repo_file_name = '/etc/apt/sources.list.d/mongodb-enterprise.list'
+        expected_content = (
+            'deb http://repo.mongodb.com/apt/{distribution} '
+            '{release}/mongodb-enterprise/3.2 multiverse'
+        ).format(
+            distribution=SystemInfo.distribution,
+            release=SystemInfo.codename
+        )
 
     repo_file = File(repo_file_name)
 
@@ -45,87 +44,35 @@ def test_enterprise_repository_file(SystemInfo, File):
     assert repo_file.user == 'root'
     assert repo_file.group == 'root'
     assert repo_file.mode == 0o644
-
-
-def test_ubuntu_community_repository_file_content(SystemInfo, File):
-    """
-    Test community repository file content on Ubuntu distributions
-    """
-
-    if SystemInfo.distribution != 'ubuntu':
-        pytest.skip('Not apply to %s' % SystemInfo.distribution)
-
-    repo_file = File('/etc/apt/sources.list.d/mongodb-org.list')
-
-    expected_content = (
-        'deb http://repo.mongodb.org/apt/{distribution} '
-        '{release}/mongodb-org/3.2 multiverse'
-    ).format(
-        distribution=SystemInfo.distribution,
-        release=SystemInfo.codename
-    )
-
     assert repo_file.contains(expected_content)
 
 
-def test_ubuntu_enterprise_repository_file_content(SystemInfo, File):
+def test_ubuntu_packages(SystemInfo, Package):
     """
-    Test enterprise repository file content on Ubuntu distributions
-    """
-
-    if SystemInfo.distribution != 'ubuntu':
-        pytest.skip('Not apply to %s' % SystemInfo.distribution)
-
-    repo_file = File('/etc/apt/sources.list.d/mongodb-enterprise.list')
-
-    expected_content = (
-        'deb http://repo.mongodb.com/apt/{distribution} '
-        '{release}/mongodb-enterprise/3.2 multiverse'
-    ).format(
-        distribution=SystemInfo.distribution,
-        release=SystemInfo.codename
-    )
-
-    assert repo_file.contains(expected_content)
-
-
-def test_ubuntu_community_packages(SystemInfo, Package):
-    """
-    Test community packages on Ubuntu distributions
+    Test packages on Ubuntu distributions
     """
 
     if SystemInfo.distribution != 'ubuntu':
         pytest.skip('Not apply to %s' % SystemInfo.distribution)
 
-    packages = [
-        'mongodb-org',
-        'mongodb-org-server',
-        'mongodb-org-mongos',
-        'mongodb-org-shell',
-        'mongodb-org-tools',
-        'python-pymongo',
-    ]
-
-    for package in packages:
-        assert Package(package).is_installed
-
-
-def test_ubuntu_enterprise_packages(SystemInfo, Package):
-    """
-    Test enterprise packages on Ubuntu distributions
-    """
-
-    if SystemInfo.distribution != 'ubuntu':
-        pytest.skip('Not apply to %s' % SystemInfo.distribution)
-
-    packages = [
-        'mongodb-enterprise',
-        'mongodb-enterprise-server',
-        'mongodb-enterprise-mongos',
-        'mongodb-enterprise-shell',
-        'mongodb-enterprise-tools',
-        'python-pymongo',
-    ]
+    if 'org' in os.getenv('MONGODB_EDITION'):
+        packages = [
+            'mongodb-org',
+            'mongodb-org-server',
+            'mongodb-org-mongos',
+            'mongodb-org-shell',
+            'mongodb-org-tools',
+            'python-pymongo',
+        ]
+    else:
+        packages = [
+            'mongodb-enterprise',
+            'mongodb-enterprise-server',
+            'mongodb-enterprise-mongos',
+            'mongodb-enterprise-shell',
+            'mongodb-enterprise-tools',
+            'python-pymongo',
+        ]
 
     for package in packages:
         assert Package(package).is_installed
@@ -166,20 +113,24 @@ def test_configuration_directory(SystemInfo, File):
 
 def test_configuration_files(SystemInfo, File):
     """
-    Test configuration files management
+    Test configuration files management on Ubuntu distributions
     """
 
     if SystemInfo.distribution != 'ubuntu':
         pytest.skip('Not apply to %s' % SystemInfo.distribution)
 
-    config_files = [
-        {'path': '/etc/mongodb/mongod_27017.conf', 'user': 'mongodb'},
-        {'path': '/etc/mongodb/mongos_27018.conf', 'user': 'bar'},
-        {'path': '/etc/mongodb/mongos_27019.conf', 'user': 'foobar'},
-        {'path': '/etc/mongodb/mongod_27020.conf', 'user': 'mongodb'},
-        {'path': '/etc/mongodb/mongos_27021.conf', 'user': 'bar'},
-        {'path': '/etc/mongodb/mongos_27022.conf', 'user': 'foobar'},
-    ]
+    if 'org' in os.getenv('MONGODB_EDITION'):
+        config_files = [
+            {'path': '/etc/mongodb/mongod_27017.conf', 'user': 'mongodb'},
+            {'path': '/etc/mongodb/mongos_27018.conf', 'user': 'bar'},
+            {'path': '/etc/mongodb/mongos_27019.conf', 'user': 'foobar'},
+        ]
+    else:
+        config_files = [
+            {'path': '/etc/mongodb/mongod_27020.conf', 'user': 'mongodb'},
+            {'path': '/etc/mongodb/mongos_27021.conf', 'user': 'bar'},
+            {'path': '/etc/mongodb/mongos_27022.conf', 'user': 'foobar'},
+        ]
 
     for current_file in config_files:
         config_file = File(current_file['path'])
@@ -212,10 +163,14 @@ def test_data_directories(SystemInfo, File):
     Test data directory management
     """
 
-    data_directories = [
-        {'path': '/var/lib/mongodb/foo', 'user': 'mongodb'},
-        {'path': '/var/lib/mongodb/foo-ent', 'user': 'mongodb'},
-    ]
+    if 'org' in os.getenv('MONGODB_EDITION'):
+        data_directories = [
+            {'path': '/var/lib/mongodb/foo', 'user': 'mongodb'},
+        ]
+    else:
+        data_directories = [
+            {'path': '/var/lib/mongodb/foo-ent', 'user': 'mongodb'},
+        ]
 
     for current_directory in data_directories:
         data_directory = File(current_directory['path'])
@@ -234,14 +189,18 @@ def test_upstart_init_files(SystemInfo, File):
     if SystemInfo.codename != 'trusty':
         pytest.skip('Not apply to %s' % SystemInfo.codename)
 
-    upstart_files = [
-        '/etc/init/mongod_27017.conf',
-        '/etc/init/mongos_27018.conf',
-        '/etc/init/mongos_27019.conf'
-        '/etc/init/mongod_27020.conf',
-        '/etc/init/mongos_27021.conf',
-        '/etc/init/mongos_27022.conf'
-    ]
+    if 'org' in os.getenv('MONGODB_EDITION'):
+        upstart_files = [
+            '/etc/init/mongod_27017.conf',
+            '/etc/init/mongos_27018.conf',
+            '/etc/init/mongos_27019.conf'
+        ]
+    else:
+        upstart_files = [
+            '/etc/init/mongod_27020.conf',
+            '/etc/init/mongos_27021.conf',
+            '/etc/init/mongos_27022.conf'
+        ]
 
     for current_file in upstart_files:
         upstart_file = File(current_file)
@@ -258,14 +217,18 @@ def test_upstart_instance_services(Command, SystemInfo, Service):
     if SystemInfo.codename != 'trusty':
         pytest.skip('Not apply to %s' % SystemInfo.codename)
 
-    services = [
-        {'name': 'mongod_27017', 'enabled': True, 'running': True},
-        {'name': 'mongos_27018', 'enabled': False, 'running': False},
-        {'name': 'mongos_27019', 'enabled': True, 'running': True},
-        {'name': 'mongod_27020', 'enabled': True, 'running': True},
-        {'name': 'mongos_27021', 'enabled': False, 'running': False},
-        {'name': 'mongos_27022', 'enabled': True, 'running': True},
-    ]
+    if 'org' in os.getenv('MONGODB_EDITION'):
+        services = [
+            {'name': 'mongod_27017', 'enabled': True, 'running': True},
+            {'name': 'mongos_27018', 'enabled': False, 'running': False},
+            {'name': 'mongos_27019', 'enabled': True, 'running': True},
+        ]
+    else:
+        services = [
+            {'name': 'mongod_27020', 'enabled': True, 'running': True},
+            {'name': 'mongos_27021', 'enabled': False, 'running': False},
+            {'name': 'mongos_27022', 'enabled': True, 'running': True},
+        ]
 
     for service in services:
         assert Service(service['name']).is_enabled is service['enabled']
@@ -285,14 +248,18 @@ def test_systemd_services_files(SystemInfo, File):
     if SystemInfo.codename != 'xenial':
         pytest.skip('Not apply to %s' % SystemInfo.codename)
 
-    services_files = [
-        '/lib/systemd/system/mongod_27017.service',
-        '/lib/systemd/system/mongos_27018.service',
-        '/lib/systemd/system/mongos_27019.service'
-        '/lib/systemd/system/mongod_27020.service',
-        '/lib/systemd/system/mongos_27021.service',
-        '/lib/systemd/system/mongos_27022.service'
-    ]
+    if 'org' in os.getenv('MONGODB_EDITION'):
+        services_files = [
+            '/lib/systemd/system/mongod_27017.service',
+            '/lib/systemd/system/mongos_27018.service',
+            '/lib/systemd/system/mongos_27019.service'
+        ]
+    else:
+        services_files = [
+            '/lib/systemd/system/mongod_27020.service',
+            '/lib/systemd/system/mongos_27021.service',
+            '/lib/systemd/system/mongos_27022.service'
+        ]
 
     for current_file in services_files:
         service_file = File(current_file)
@@ -309,14 +276,18 @@ def test_systemd_instance_services(Command, SystemInfo, Service):
     if SystemInfo.codename != 'xenial':
         pytest.skip('Not apply to %s' % SystemInfo.codename)
 
-    services = [
-        {'name': 'mongod_27017', 'enabled': True, 'running': True},
-        {'name': 'mongos_27018', 'enabled': False, 'running': False},
-        {'name': 'mongos_27019', 'enabled': True, 'running': True},
-        {'name': 'mongod_27020', 'enabled': True, 'running': True},
-        {'name': 'mongos_27021', 'enabled': False, 'running': False},
-        {'name': 'mongos_27022', 'enabled': True, 'running': True},
-    ]
+    if 'org' in os.getenv('MONGODB_EDITION'):
+        services = [
+            {'name': 'mongod_27017', 'enabled': True, 'running': True},
+            {'name': 'mongos_27018', 'enabled': False, 'running': False},
+            {'name': 'mongos_27019', 'enabled': True, 'running': True},
+        ]
+    else:
+        services = [
+            {'name': 'mongod_27020', 'enabled': True, 'running': True},
+            {'name': 'mongos_27021', 'enabled': False, 'running': False},
+            {'name': 'mongos_27022', 'enabled': True, 'running': True},
+        ]
 
     for service in services:
         assert Service(service['name']).is_enabled is service['enabled']
