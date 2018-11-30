43c28099805206a953ae5753924be6578aa9065e
Alexandre Chaussier <alexandre.chaussier@metronlab.com>
2018-11-30 06:03:13 +0100

    Add new OS releases management

--- molecule/default/tests/test_installation.py
+++ molecule/default/tests/test_installation.py
@@ -11,31 +11,38 @@ testinfra_hosts = AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_repository_file(host):
+def test_repository_file_debian_family(host):
     """
     Test repository file permissions and content on Ubuntu distributions
     """
 
-    if host.system_info.distribution != 'ubuntu':
+    if host.system_info.distribution not in ['debian', 'ubuntu']:
         pytest.skip('Not apply to %s' % host.system_info.distribution)
+
+    if host.system_info.distribution == 'debian':
+        section_name = 'main'
+    else:
+        section_name = 'multiverse'
 
     if 'mongodb-org' in host.check_output('cat /etc/hostname'):
         repo_file_name = '/etc/apt/sources.list.d/mongodb-org.list'
         expected_content = (
-            'deb http://repo.mongodb.org/apt/{distribution} '
-            '{release}/mongodb-org/3.4 multiverse'
+            'deb https://repo.mongodb.org/apt/{distribution} '
+            '{release}/mongodb-org/4.0 {section_name}'
         ).format(
             distribution=host.system_info.distribution,
-            release=host.system_info.codename
+            release=host.system_info.codename,
+            section_name=section_name
         )
     else:
         repo_file_name = '/etc/apt/sources.list.d/mongodb-enterprise.list'
         expected_content = (
-            'deb http://repo.mongodb.com/apt/{distribution} '
-            '{release}/mongodb-enterprise/3.4 multiverse'
+            'deb https://repo.mongodb.com/apt/{distribution} '
+            '{release}/mongodb-enterprise/4.0 {section_name}'
         ).format(
             distribution=host.system_info.distribution,
-            release=host.system_info.codename
+            release=host.system_info.codename,
+            section_name=section_name
         )
 
     repo_file = host.file(repo_file_name)
@@ -48,33 +55,24 @@ def test_repository_file(host):
     assert repo_file.contains(expected_content)
 
 
-def test_ubuntu_packages(host):
+@pytest.mark.parametrize('pattern', [
+    'mongodb-{}',
+    'mongodb-{}-server',
+    'mongodb-{}-mongos',
+    'mongodb-{}-shell',
+    'mongodb-{}-tools',
+])
+def test_packages(host, pattern):
     """
-    Test packages on Ubuntu distributions
+    Test packages
     """
-
-    if host.system_info.distribution != 'ubuntu':
-        pytest.skip('Not apply to %s' % host.system_info.distribution)
 
     if 'mongodb-org' in host.check_output('cat /etc/hostname'):
-        packages = [
-            'mongodb-org',
-            'mongodb-org-server',
-            'mongodb-org-mongos',
-            'mongodb-org-shell',
-            'mongodb-org-tools',
-        ]
+        package_name = pattern.format('org')
     else:
-        packages = [
-            'mongodb-enterprise',
-            'mongodb-enterprise-server',
-            'mongodb-enterprise-mongos',
-            'mongodb-enterprise-shell',
-            'mongodb-enterprise-tools',
-        ]
+        package_name = pattern.format('enterprise')
 
-    for package in packages:
-        assert host.package(package).is_installed
+    assert host.package(package_name).is_installed
 
 
 def test_pymongo_management(host):
@@ -87,20 +85,11 @@ def test_pymongo_management(host):
 
 def test_default_instance_service(host):
     """
-    Test default instance service disabled on Ubuntu distributions
+    Test default instance service disabled
     """
 
-    if host.system_info.distribution != 'ubuntu':
-        pytest.skip('Not apply to %s' % host.system_info.distribution)
-
     assert host.service('mongod').is_enabled is False
-
-    if host.system_info.codename == 'trusty':
-        # On my Trusty service test, return code is 0 when service stopped
-        expected_output = 'mongod stop/waiting'
-        assert expected_output in host.check_output('service mongod status')
-    else:
-        assert host.service('mongod').is_running is False
+    assert host.service('mongod').is_running is False
 
 
 def test_configuration_directory(host):
@@ -108,161 +97,115 @@ def test_configuration_directory(host):
     Test configuration directory management
     """
 
-    if host.system_info.distribution != 'ubuntu':
-        pytest.skip('Not apply to %s' % host.system_info.distribution)
+    if host.system_info.distribution in ['debian', 'ubuntu']:
+        mongodb_group = 'mongodb'
+    else:
+        mongodb_group = 'mongod'
 
     config_dir = host.file('/etc/mongodb')
     assert config_dir.exists
     assert config_dir.is_directory
     assert config_dir.user == 'root'
-    assert config_dir.group == 'mongodb'
+    assert config_dir.group == mongodb_group
     assert config_dir.mode == 0o750
 
 
-def test_configuration_files(host):
+@pytest.mark.parametrize('path,user', [
+    ('/etc/mongodb/mongod_27017.conf', 'mongodb'),
+    ('/etc/mongodb/mongod_27017.key', 'mongodb'),
+    ('/etc/mongodb/mongod_27018.conf', 'bar'),
+    ('/etc/mongodb/mongod_27018.key', 'bar'),
+    ('/etc/mongodb/mongos_27019.conf', 'foobar'),
+    ('/etc/mongodb/mongos_27019.key', 'foobar'),
+])
+def test_configuration_files(host, path, user):
     """
-    Test configuration files management on Ubuntu distributions
+    Test configuration files management
     """
 
-    if host.system_info.distribution != 'ubuntu':
-        pytest.skip('Not apply to %s' % host.system_info.distribution)
+    if host.system_info.distribution in ['debian', 'ubuntu']:
+        mongodb_user = 'mongodb'
+    else:
+        mongodb_user = 'mongod'
 
-    config_files = [
-        {'path': '/etc/mongodb/mongod_27017.conf', 'user': 'mongodb'},
-        {'path': '/etc/mongodb/mongod_27017.key', 'user': 'mongodb'},
-        {'path': '/etc/mongodb/mongod_27018.conf', 'user': 'bar'},
-        {'path': '/etc/mongodb/mongod_27018.key', 'user': 'bar'},
-        {'path': '/etc/mongodb/mongos_27019.conf', 'user': 'foobar'},
-        {'path': '/etc/mongodb/mongos_27019.key', 'user': 'foobar'},
-    ]
+    if user != 'mongodb':
+        mongodb_user = user
 
-    for current_file in config_files:
-        config_file = host.file(current_file['path'])
-        assert config_file.exists
-        assert config_file.is_file
-        assert config_file.user == current_file['user']
-        assert config_file.group == current_file['user']
-        assert config_file.mode == 0o400
+    config_file = host.file(path)
+    assert config_file.exists
+    assert config_file.is_file
+    assert config_file.user == mongodb_user
+    assert config_file.group == mongodb_user
+    assert config_file.mode == 0o400
 
 
-def test_instance_users(host):
+@pytest.mark.parametrize('user', [('bar'), ('foobar')])
+def test_instance_users(host, user):
     """
     Test dedicated instance user
     """
 
-    users = ['bar', 'foobar']
+    if host.system_info.distribution in ['debian', 'ubuntu']:
+        mongodb_user = 'mongodb'
+    else:
+        mongodb_user = 'mongod'
 
-    for user in users:
-        current_user = host.user(user)
+    current_user = host.user(user)
 
-        assert current_user.exists
-        assert current_user.group == user
-        assert 'mongodb' in current_user.groups
-        assert current_user.home == '/home/%s' % user
-        assert current_user.shell == '/bin/false'
+    assert current_user.exists
+    assert current_user.group == user
+    assert mongodb_user in current_user.groups
+    assert current_user.home == '/home/%s' % user
+    assert current_user.shell == '/bin/false'
 
 
-def test_data_directories(host):
+@pytest.mark.parametrize('path', ['/var/lib/mongodb/foo'])
+def test_data_directories(host, path):
     """
     Test data directory management
     """
 
-    data_directories = [
-        {'path': '/var/lib/mongodb/foo', 'user': 'mongodb'},
-    ]
+    if host.system_info.distribution in ['debian', 'ubuntu']:
+        mongodb_user = 'mongodb'
+    else:
+        mongodb_user = 'mongod'
 
-    for current_directory in data_directories:
-        data_directory = host.file(current_directory['path'])
-        assert data_directory.exists
-        assert data_directory.is_directory
-        assert data_directory.user == current_directory['user']
-        assert data_directory.group == current_directory['user']
-        assert data_directory.mode == 0o750
-
-
-def test_upstart_init_files(host):
-    """
-    Test upstart init files management
-    """
-
-    if host.system_info.codename != 'trusty':
-        pytest.skip('Not apply to %s' % host.system_info.codename)
-
-    upstart_files = [
-        '/etc/init/mongod_27017.conf',
-        '/etc/init/mongod_27018.conf',
-        '/etc/init/mongos_27019.conf'
-    ]
-
-    for current_file in upstart_files:
-        upstart_file = host.file(current_file)
-        assert upstart_file.exists
-        assert upstart_file.is_file
-        assert upstart_file.user == 'root'
+    data_directory = host.file(path)
+    assert data_directory.exists
+    assert data_directory.is_directory
+    assert data_directory.user == mongodb_user
+    assert data_directory.group == mongodb_user
+    assert data_directory.mode == 0o750
 
 
-def test_upstart_instance_services(host):
-    """
-    Test instance upstart services on Ubuntu Trusty
-    """
-
-    if host.system_info.codename != 'trusty':
-        pytest.skip('Not apply to %s' % host.system_info.codename)
-
-    services = [
-        {'name': 'mongod_27017', 'enabled': True, 'running': True},
-        {'name': 'mongod_27018', 'enabled': False, 'running': False},
-        {'name': 'mongos_27019', 'enabled': True, 'running': True},
-    ]
-
-    for service in services:
-        assert host.service(service['name']).is_enabled is service['enabled']
-        if service['running']:
-            assert '%s start/running' % service['name'] in \
-                host.check_output('service %s status' % service['name'])
-        else:
-            assert '%s stop/waiting' % service['name'] in \
-                host.check_output('service %s status' % service['name'])
-
-
-def test_systemd_services_files(host):
+@pytest.mark.parametrize('path', [
+    ('/lib/systemd/system/mongod_27017.service'),
+    ('/lib/systemd/system/mongod_27018.service'),
+    ('/lib/systemd/system/mongos_27019.service')
+])
+def test_systemd_services_files(host, path):
     """
     Test systemd services files management
     """
 
-    if host.system_info.codename != 'xenial':
-        pytest.skip('Not apply to %s' % host.system_info.codename)
-
-    services_files = [
-        '/lib/systemd/system/mongod_27017.service',
-        '/lib/systemd/system/mongod_27018.service',
-        '/lib/systemd/system/mongos_27019.service'
-    ]
-
-    for current_file in services_files:
-        service_file = host.file(current_file)
-        assert service_file.exists
-        assert service_file.is_file
-        assert service_file.user == 'root'
+    service_file = host.file(path)
+    assert service_file.exists
+    assert service_file.is_file
+    assert service_file.user == 'root'
 
 
-def test_systemd_instance_services(host):
+@pytest.mark.parametrize('service_name,enabled,running', [
+    ('mongod_27017', True, True),
+    ('mongod_27018', False, False),
+    ('mongos_27019', True, True)
+])
+def test_systemd_instance_services(host, service_name, enabled, running):
     """
-    Test instance systemd services on Ubuntu Xenial
+    Test instance systemd services
     """
 
-    if host.system_info.codename != 'xenial':
-        pytest.skip('Not apply to %s' % host.system_info.codename)
-
-    services = [
-        {'name': 'mongod_27017', 'enabled': True, 'running': True},
-        {'name': 'mongod_27018', 'enabled': False, 'running': False},
-        {'name': 'mongos_27019', 'enabled': True, 'running': True},
-    ]
-
-    for service in services:
-        assert host.service(service['name']).is_enabled is service['enabled']
-        assert host.service(service['name']).is_running is service['running']
+    assert host.service(service_name).is_enabled is enabled
+    assert host.service(service_name).is_running is running
 
 
 def test_hugepage_service_file(host):
@@ -288,21 +231,16 @@ def test_hugepage_service_state(host):
     assert service.is_running
 
 
-def test_hugepage_setting_value(host):
+@pytest.mark.parametrize('path', [
+    ('/sys/kernel/mm/transparent_hugepage/enabled'),
+    ('/sys/kernel/mm/transparent_hugepage/defrag'),
+])
+def test_hugepage_setting_value(host, path):
     """
     Test if kernel hugepage setting have recommended value
     """
 
-    if host.system_info.distribution != 'ubuntu':
-        pytest.skip('Not apply to %s' % host.system_info.distribution)
-
-    setting_paths = [
-        '/sys/kernel/mm/transparent_hugepage/enabled',
-        '/sys/kernel/mm/transparent_hugepage/defrag',
-    ]
-
-    for setting_path in setting_paths:
-        assert '[never]' in host.file(setting_path).content_string
+    assert '[never]' in host.file(path).content_string
 
 
 def test_logrotate_file(host):
@@ -316,23 +254,20 @@ def test_logrotate_file(host):
     assert config_file.user == 'root'
 
 
-def test_mongodb_users(host):
+@pytest.mark.parametrize('user,database', [
+    ('foo', 'admin'),
+    ('foobar', 'foobar_db'),
+])
+def test_mongodb_users(host, user, database):
     """
     Test if MongoDB users created
     """
 
-    users = [
-        {'name': 'foo', 'database': 'admin'},
-        {'name': 'foobar', 'database': 'foobar_db'},
-    ]
-
     test_host = 'localhost:27017'
 
-    for user in users:
-        result = json.loads(host.check_output(
-            "mongo {}/{} --quiet --eval 'printjson(db.getUsers())'".format(
-                test_host, user['database'])))
-        assert result[0]['_id'] == "{}.{}".format(
-            user['database'], user['name'])
-        assert result[0]['db'] == user['database']
-        assert result[0]['user'] == user['name']
+    result = json.loads(host.check_output(
+        "mongo {}/{} --quiet --eval 'printjson(db.getUsers())'".format(
+            test_host, database)))
+    assert result[0]['_id'] == "{}.{}".format(database, user)
+    assert result[0]['db'] == database
+    assert result[0]['user'] == user
